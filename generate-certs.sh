4726a5ed1f5c18d8f7868cc1e4a2c5460b18816c
bobby jenrow <bobbyjenrow@gmail.com>
2022-07-09 14:35:39 -0400

    add scripts

---
+++ generate-certs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "We need to generate certificates for local https"
+mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "docker.localhost" "*.docker.localhost" "domain.local" "*.domain.local"
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Set up required proxy network 'web'"
+docker network create proxy
+
